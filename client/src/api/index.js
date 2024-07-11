fcf417b126402142471c0f65873271ec6806c8ee
GOVIND <govind007india@gmail.com>
2024-07-11 11:47:01 +0530

    deploy update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://govind-nodejs.azurewebsites.net/",
 });
 
 API.interceptors.request.use((req) => {
--- server/index.js
+++ server/index.js
@@ -3,52 +3,30 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import userRoutes from "./routes/users.js";
-import questionRoutes from "./routes/Questions.js";
-import answerRoutes from "./routes/Answers.js";
+ import userRoutes from "./routes/users.js";
+ import questionRoutes from "./routes/Questions.js";
+ import answerRoutes from "./routes/Answers.js";
+ import connectDB from "./connectMongoDb.js";
 
-dotenv.config();
-
+ dotenv.config();
+// connectDB();
 const app = express();
-
-// Middleware
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-// CORS configuration
-const corsOptions = {
-  origin: "*", // Ideally, set specific origins in production
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+ app.get('/',(req, res) => {
+     res.send("This is a stack overflow clone API")
+ })
 
-// Routes
-app.get('/', (req, res) => {
-  res.send("This is a stack overflow clone API");
-});
-
-app.use("/user", userRoutes);
-app.use("/questions", questionRoutes);
-app.use("/answer", answerRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+ app.use("/user", userRoutes);
+ app.use("/questions", questionRoutes);
+ app.use("/answer", answerRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-// Use environment variable for database connection
-const CONNECTION_URL = process.env.MONGODB_URI;
+const CONNECTION_URL = "mongodb+srv://admin:admin@stackoverflow-clone.uyhchiv.mongodb.net/?retryWrites=true&w=majority&appName=StackOverflow-Clone"
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MongoDB connection error: ", err.message);
-    process.exit(1);
-  });
+mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
+  .catch((err) => console.log(err.message))
